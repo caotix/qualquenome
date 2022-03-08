4fdd543ec8a658060abedc0f5d177550d49103c0
caotix <99762830+caotix@users.noreply.github.com>
2022-03-08 17:35:37 -0400

    Add files via upload

--- funcoes.js
+++ funcoes.js
@@ -1,15 +1,44 @@
-var numSort = []
-
+var numsort = []
+var numdig = []
 function sorteioNum(){
-    numSort = []
+    numsort = []
     do{
 
     
     let sort = Math.ceil (Math.random()*60)
-    if(!numSort.includes(sort))
-    numSort.push(sort)
+    if(!numsort.includes(sort))
+    numsort.push(sort)
     
-    }while(numSort.length<6);
+    }while(numsort.length<6);
 
-    console.log(sort)
+    document.getElementById("numsort").innerHTML = numsort
+}
+
+function getNum(valor,pos){
+    if(valor>60){
+        alert("o valor é maior que 60")
+        limpar(pos)
+    }else if(valor <= 0){
+    alert("o valor não pode ser menor que 0")
+    limpar(pos)
+    }else if(numdig.includes(valor)){
+      alert("o Valor ja foi digitado")
+      limpar(pos)
+    }else{
+        numdig[pos]=valor
+    }
+}
+
+function verificaacertos(){
+    let cont = 0
+    numdig.forEach((valor, index) => {
+            if (numsort.includes(valor)) {
+                cont = cont + 1;
+            }
+        })
+    document.getElementById("total").innerText = cont
+}
+
+function limpar(pos){
+    document.getElementById(`num${pos}`).value = ""
 }
